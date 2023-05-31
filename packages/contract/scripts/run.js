f5ea0d1fc5cb1f51d4646f82f513810da63e8bde
github honganji <yujicavs@icloud.com>
2023-05-31 11:45:43 +0900

    add Base64 function and basic game system

--- packages/contract/scripts/run.js
+++ packages/contract/scripts/run.js
@@ -2,11 +2,29 @@ const hre = require('hardhat');
 
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-  const gameContract = await gameContractFactory.deploy();
-  const nftGame = await gameContract.deployed();
+  const gameContract = await gameContractFactory.deploy(
+    ['ZORO', 'NAMI', 'USOPP'], // キャラクターの名前
+    [
+      'https://i.imgur.com/TZEhCTX.png', // キャラクターの画像
+      'https://i.imgur.com/WVAaMPA.png',
+      'https://i.imgur.com/pCMZeiM.png',
+    ],
+    [100, 200, 300], // キャラクターのHP
+    [100, 50, 25], // キャラクターの攻撃力
+  );
+  await gameContract.deployed();
+  console.log('Contract deployed to:', gameContract.address);
+  // 3体のNFTキャラクターの中から、3番目のキャラクターを Mint しています。
+  const txn = await gameContract.mintCharacterNFT(2);
 
-  console.log('Contract deployed to:', nftGame.address);
+  // Minting が仮想マイナーにより、承認されるのを待ちます。
+  await txn.wait();
+
+  // NFTのURIの値を取得します。tokenURI は ERC721 から継承した関数です。
+  const returnedTokenUri = await gameContract.tokenURI(1);
+  console.log('Token URI:', returnedTokenUri);
 };
+
 const runMain = async () => {
   try {
     await main();
@@ -15,4 +33,5 @@ const runMain = async () => {
     throw new Error('Something bad happened!');
   }
 };
+
 runMain();
